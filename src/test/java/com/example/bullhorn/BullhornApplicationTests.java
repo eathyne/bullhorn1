88a27cb1950410aa571bb2c6a27a01c015456275
Eathyn Edwards <eathynedwards@Earyns-MacBook-Air.local>
2019-12-09 17:01:53 -0500

    Initial commit

---
+++ src/test/java/com/example/bullhorn/BullhornApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bullhorn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullhornApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
